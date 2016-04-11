d44d7938ca9c69395843bb5854b86a47d29a56f0
Jean Pasdeloup <jean.pasdeloup@obspm.fr>
2016-04-11 23:03:38 +0200

    add publish scripts

---
+++ bin/publishMeudon.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+app/console deus:publish_simulation 162 1024 sucdmw5 
+app/console deus:publish_simulation 162 512 lcdmw5 
+app/console deus:publish_simulation 162 1024 rpcdmw5 
+app/console deus:publish_simulation 162 512 rpcdmw5 
+app/console deus:publish_simulation 162 1024 lcdmw5 
+app/console deus:publish_simulation 648 1024 rpcdmw5 
+app/console deus:publish_simulation 648 2048 wcdmw7 
+app/console deus:publish_simulation 648 2048 rpcdmw5 
+app/console deus:publish_simulation 648 2048 lcdmw7 
+app/console deus:publish_simulation 648 2048 rpcdmw7 
+app/console deus:publish_simulation 648 1024 sucdmw5 
+app/console deus:publish_simulation 648 1024 lcdmw5 
+app/console deus:publish_simulation 2592 1024 rpcdmw5 
+app/console deus:publish_simulation 2592 2048 rpcdmw5 
+app/console deus:publish_simulation 2592 1024 lcdmw5 
+app/console deus:publish_simulation 2592 1024 sucdmw5 
+app/console deus:publish_simulation 2592 2048 lcdmw5 
+app/console deus:publish_simulation 5184 2048 lcdmw5 
+app/console deus:publish_simulation 5184 2048 rpcdmw5 
+app/console deus:publish_simulation 10500 4096 rpcdmw7 
+app/console deus:publish_simulation 10500 4096 lcdmw7 
+app/console deus:publish_simulation 10500 4096 wcdmw7 
+app/console deus:publish_simulation 21000 8192 rpcdmw7 
+app/console deus:publish_simulation 21000 8192 wcdmw7 
+app/console deus:publish_simulation 21000 8192 lcdmw7 
+
---
+++ bin/resetAndReimportAll.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+bin/reset.sh
+bin/import_AllMeudon.sh
+bin/import_AllExternal.sh
+bin/publishMeudon.sh
+
