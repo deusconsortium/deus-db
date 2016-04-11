0a0f26b7625cfcad5ef29610dd0ba5715cd321fe
Jean Pasdeloup <jean.pasdeloup@obspm.fr>
2016-04-11 22:22:05 +0200

    update import scripts

---
+++ bin/import_AllExternal.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+app/console deusdb:import_index data/idris_ergon_storedir ergon
+app/console deusdb:import_index data/tgcc_curie_storedir curie
---
+++ bin/import_AllMeudon.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+app/console deusdb:import_index data/meudon_bingo_data
+app/console deusdb:import_index data/meudon_efiler_data1_deus_data
+app/console deusdb:import_index data/meudon_efiler_data1_Babel_le
+app/console deusdb:import_index data/meudon_efiler_data1_deus_fur
+
